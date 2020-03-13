fbd06760a43adfc592ba3723384eec7bb3681b8f
Jonathan <jonathanpalma89@gmail.com>
2020-03-13 19:38:49 +0000

    chore: add Position Class

---
+++ Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Chess.board;
+
+namespace Chess
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            Position position = new Position(3, 4); 
+
+            Console.WriteLine($"Position: {position}");
+        }
+    }
+}
